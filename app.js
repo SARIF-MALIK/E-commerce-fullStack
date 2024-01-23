ebd670369dc3df55ba82a8f11546ccf3236d2bc1
SARIF <sarif.malik@hotmail.com>
2024-01-23 16:13:49 +0530

    AUTHENTICATION SCHEMA PASSPORT

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const mongoose = require('mongoose');
 const path = require('path'); 
 const seedDB = require('./seed'); 
 var methodOverride = require('method-override')
-const productRoutes = require('./routes/product');
-const reviewRoutes = require('./routes/reviews'); 
 const session = require('express-session')
 const flash = require('connect-flash')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const User = require('./models/User')
+
+const productRoutes = require('./routes/product');
+const reviewRoutes = require('./routes/reviews'); 
+const authRoutes = require('./routes/auth'); 
 
 mongoose.set('strictQuery', true); 
 mongoose.connect('mongodb://127.0.0.1:27017/websoldiers')  // database name websoldiers 
@@ -31,6 +36,13 @@ let configSession = {
 
 app.use(session(configSession));
 app.use(flash());
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req,res,next)=>{
     res.locals.success = req.flash('success');
@@ -38,8 +50,21 @@ app.use((req,res,next)=>{
     next();
 })
 
+// passport 
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+      User.findOne({ username: username }, function (err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        if (!user.verifyPassword(password)) { return done(null, false); }
+        return done(null, user);
+      });
+    }
+  ));
+
 app.use(productRoutes); 
 app.use(reviewRoutes);
+app.use(authRoutes); 
 
 let PORT = 8080; 
 app.listen(PORT, ()=>{
