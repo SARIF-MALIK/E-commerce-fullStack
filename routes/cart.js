98f02159bc11370bfb685717284c6cb75aab789e
SARIF <sarif.malik@hotmail.com>
2024-01-29 13:34:12 +0530

    cart display n error resolved using populate

--- routes/cart.js
+++ routes/cart.js
@@ -7,21 +7,26 @@ const User = require('../models/User');
 // route to see the cart 
 
 router.get('/user/cart', isLoggedIn, async(req, res)=>{
-    let user = await User.findById(req.user._id); 
-    res.render('cart', {user}); 
+    let user = await User.findById(req.user._id).populate('cart'); 
+    res.render('cart/cart', {user}); 
 })
 
 
 // adding the product to the cart 
 router.post('/user/:productId/add', isLoggedIn, async(req, res)=>{
-    let {productId} = req.params; 
-    let userId = req.user._id; 
-
-    let product = await Product.findById(productId); 
-    let user = await User.findById(userId); 
-    user.cart.push(product); 
+    try {
+        let {productId} = req.params; 
+        let userId = req.user._id; 
     
-    res.redirect('/user/cart'); 
+        let product = await Product.findById(productId); 
+        let user = await User.findById(userId); 
+        user.cart.push(product); 
+        await user.save(); 
+        res.redirect('/user/cart'); 
+    } catch (e) {
+        res.status(500).render('error', { err: e.message });
+    }
+   
 })
 
 
