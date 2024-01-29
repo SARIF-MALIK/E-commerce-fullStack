2db04b44c7f2d28d07a1ac968ff706f3b935c93b
SARIF <sarif.malik@hotmail.com>
2024-01-29 12:57:43 +0530

    cart route and schema

--- routes/cart.js
+++ routes/cart.js
@@ -1,7 +1,28 @@
 const express = require('express'); 
 const router = express.Router(); 
+const {isLoggedIn} = require('../middleware');
+const Product = require('../models/Product');
+const User = require('../models/User');
+
+// route to see the cart 
+
+router.get('/user/cart', isLoggedIn, async(req, res)=>{
+    let user = await User.findById(req.user._id); 
+    res.render('cart', {user}); 
+})
 
 
+// adding the product to the cart 
+router.post('/user/:productId/add', isLoggedIn, async(req, res)=>{
+    let {productId} = req.params; 
+    let userId = req.user._id; 
+
+    let product = await Product.findById(productId); 
+    let user = await User.findById(userId); 
+    user.cart.push(product); 
+    
+    res.redirect('/user/cart'); 
+})
 
 
 module.exports = router; 
